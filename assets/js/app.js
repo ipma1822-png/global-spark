79eda6bd7be6ff90b0f71b5fb376be21dbb2d714
jeonseongkweon-cloud <jeonseongkweon@gmail.com>
2026-09-03 11:29:12 +0900

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,3 @@
+// GLOBAL SPARK HQ v0.1.0
+// No fabricated live metrics: real values will be connected to the independent GLOBAL SPARK backend.
+document.documentElement.dataset.sparkVersion='0.1.0';
